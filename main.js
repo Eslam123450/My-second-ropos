fe0d4a93d30007d1a8a8495b31226b0b112d6854
Eslam Ibrahem <aa7917478@gmail.com>
2026-04-23 01:28:56 +0300

    add nav-bar ui

---
+++ main.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", (e) => {
+    // نتأكد إن اللي اتضغط عليه هو link
+    if (e.target.tagName === "A") {
+        
+        const links = menu.querySelectorAll("a");
+
+        links.forEach(link => {
+            link.classList.remove("active");
+        });
+
+        e.target.classList.add("active");
+    }
+});
